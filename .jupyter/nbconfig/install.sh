e786ffafcdc4f267b4557c51d3889ec7722bf0d8
gaebor <gaebor@users.noreply.github.com>
2022-01-18 12:26:34 +0100

    add commit about dependency

--- .jupyter/nbconfig/install.sh
+++ .jupyter/nbconfig/install.sh
@@ -1,3 +1,5 @@
+# requires black to be installed
+
 jupyter contrib nbextension install --user
 jupyter nbextension enable code_prettify/code_prettify
 mv notebook.json $(jupyter --config-dir)/nbconfig/notebook.json
